a1720b7e00353bfa29557ea3ffd88d3380cac83b
Nenad <baltic.nenad@gmail.com>
2020-12-01 15:26:44 +0100

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/HomePage";
+import ShopPage from "./pages/shop/Shop";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
--- src/components/menu-item/MenuItem.js
+++ src/components/menu-item/MenuItem.js
@@ -4,7 +4,10 @@ import "./MenuItem.scss";
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
-    <div className={`${size} menu-item`}>
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
